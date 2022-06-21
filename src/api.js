6528e583f0621365b0e01f0ed6652cad0efd4c2a
Lucantin <luccabento@gmail.com>
2022-06-20 23:54:08 -0300

    api try catch

--- src/api.js
+++ src/api.js
@@ -7,3 +7,22 @@ export const searchPokemon = async (pokemon) => {
         console.log(error);
     }
 }
+
+export const getPokemons = async (Limit = 50, offset = 0) => {
+    try {
+        let url = `https://pokeapi.co/api/v2/pokemon/?limit=${Limit}&offset=${offset}`;
+        const response = await fetch(url);
+        return await response.json()
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getPokemonData = async (url) => {
+    try {
+        const response = await fetch(url);
+        return await response.json()
+    } catch (error) {
+        console.log(error);
+    }
+}
