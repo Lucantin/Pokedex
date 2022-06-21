9eb8b247391f713a9680eb138a6a028ababd96b7
Lucantin <luccabento@gmail.com>
2022-06-20 23:55:26 -0300

    useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { getPokemonData, getPokemons } from "./api";
 import './App.css';
 import Navbar from './components/Navbar';
 import Pokedex from "./components/Pokedex";
@@ -6,11 +7,35 @@ import SearchBar from './components/SearchBar';
 
 function App() {
 
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async () => {
+    try{
+    setLoading(true);
+    const data = await getPokemons();
+    const promises = data.results.map(async (pokemon) => {
+      return await getPokemonData(pokemon.url)
+    });
+
+    const results = await Promise.all(promises);
+    setPokemons(results);
+    setLoading(false);
+    } catch (error) {
+      console.log("fetchPokemons error: " ,error);
+    }
+  }
+
+  useEffect(() => {
+    console.log("carregou")
+    fetchPokemons();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <SearchBar/>
-      <Pokedex />
+      <Pokedex pokemons={pokemons} loading={loading}/>
     </div>
   );
 }
